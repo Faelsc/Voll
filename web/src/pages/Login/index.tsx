d299c41063d60e4df86212af769c16251497aeb1
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-09-14 14:37:35 -0300

    :sparkles: feat: criando autenticação :lipstick: feat: cabeçalho dinâmico

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import CampoDigitacao from "../../components/CampoDigitacao";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Botao from "../../components/Botao";
 import logo from './Logo.png';
 import usePost from "../../usePost";
+import autenticaStore from "../../stores/autentica.store";
 
 const Imagem = styled.img`
   padding: 2em 0;
@@ -53,7 +54,8 @@ interface ILogin {
 export default function Login() {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
-    const {cadastrarDados, erro, sucesso} = usePost();
+    const {cadastrarDados, erro, sucesso, resposta} = usePost();
+    const navigate = useNavigate();
 
     const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
@@ -64,6 +66,8 @@ export default function Login() {
 
       try {
         cadastrarDados({ url: "auth/login", dados:usuario})
+        autenticaStore.login({email: email, token: resposta})
+        resposta && navigate('/dashboard')
       } catch (erro) {
         erro && alert('Não foi possível fazer login')
       }
